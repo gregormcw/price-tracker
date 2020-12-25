fd1adf4f507be30faafc327c4be14e6cfbba1a72
Gregor McWilliam <gregormcwilliam@McDubs-MBP.local>
2020-12-25 17:16:18 -0500

    Main functions added

--- price_tracker.py
+++ price_tracker.py
@@ -1,2 +1,54 @@
-def price_tracker():
-    pass
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+
+def send_mail(email_ad, pw, to_ad):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(email_ad, pw)
+    subject = "{} is now available for {}: at or below your threshold price!".format(title, price)
+    body = "Click the link to make your purchase: {}".format(URL)
+    msg = f"subject: {subject}\n\n{body}"
+    server.sendmail(email_ad, to_ad, msg)
+    print("Email sent!")
+    server.quit()
+
+
+def price_tracker(URL, headers, threshold, email_ad=None, pw=None, to_ad=None):
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    title = soup.find("div", {"class": "title"}).get_text()
+    price = soup.find("div", {"class": "price"}).get_text()
+
+    if price[0] == "$":
+        currency = "USD"
+
+    if float(price[1:]) <= float(threshold):
+        print("Deal time!")
+        if email_ad and pw and to_ad:
+            send_mail(email_ad, pw, to_ad)
+
+    else:
+        print("Not today, bruh...")
+
+    print(title)
+    print(price)
+    print(currency)
+
+
+# URL = "https://www.dickssportinggoods.com/p/carhartt-a18-watch-hat-15cttmcrylcwtchcpapa/15cttmcrylcwtchcpapa?color=" \
+#       "Carhartt%20Brown"
+
+URL = "https://www.dickssportinggoods.com/p/strikeforce-linds-heritage-mens-performance-bowling-shoes-20sfcmlndshrtgr" \
+      "hbstg/20sfcmlndshrtgrhbstg"
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                         "Version/14.0.1 Safari/605.1.15"}
+
+# threshold = 15.0
+# currency = 0
+#
+# price_tracker(URL, headers, threshold)
